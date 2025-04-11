b4dba5150c60c7b28194fe00b7cc5ed0496a788f
Lujam2299 <21690001@tecvalles.mx>
2025-04-11 10:21:40 -0600

    creación del primer modelo de la bd

---
+++ app/Models/SolicitudAlta.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SolicitudAlta extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        //id_supervisor
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'fecha_nacimiento',
+        'curp',
+        'rfc',
+        'nss',
+        'estado_civil',
+        'domicilio_calle',
+        'domicilio_numero',
+        'domicilio_colonia',
+        'domicilio_ciudad',
+        'domicilio_estado',
+        'telefono',
+        'email',
+        'status',
+        'observaciones',
+        'rol',
+        'created_at',
+        'updated_at',
+    ];
+}
