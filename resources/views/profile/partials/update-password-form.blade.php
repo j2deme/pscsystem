feea02674346d05a9ce9ca37d8ebc083b624e591
Lujam2299 <lujam2299@gmail.com>
2025-04-23 15:49:49 -0600

    ajustes a pestaña de editar perfil

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -33,6 +33,9 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <a href="{{ route('dashboard') }}" class="inline-block bg-gray-300 text-gray-800 py-2 px-4 rounded-md hover:bg-gray-400 mt-3 mr-2 mb-2">
+                Regresar
+            </a>
 
             @if (session('status') === 'password-updated')
                 <p
