b9564f485655bf4d54a5022e7f884361d533e6c6
Lujam2299 <lujam2299@gmail.com>
2025-05-14 16:42:12 -0600

    Ajuste a login visual

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-blue-50 dark:bg-gray-900">
+            <div class="mb-6">
 
+            </div>
 
 
             <div class="w-full sm:max-w-md px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
