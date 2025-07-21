7254bb3455057f62f01e6c37b90f39c4b238af72
Lujam2299 <21690001@tecvalles.mx>
2025-07-21 16:18:47 -0600

    Panel de mensajería re ocultado para panel rh

--- resources/views/components/rh-navbar.blade.php
+++ resources/views/components/rh-navbar.blade.php
@@ -99,12 +99,6 @@
                     'icono' => 'users',
                     'color' => 'bg-pink-100 dark:bg-pink-700'
                 ],
-                [
-            'titulo' => 'Mensajes',
-            'ruta' => route('mensajes.index'),
-            'icono' => 'message',
-            'color' => 'bg-purple-300 dark:bg-purple-700',
-        ],
             ];
         @endphp
 
